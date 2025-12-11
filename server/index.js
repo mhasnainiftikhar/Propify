0d37aba6852ca6082414307d70299e21269d8fae
Muhammad Hasnain Iftikhar <160528829+mhasnainiftikhar@users.noreply.github.com>
2025-12-11 15:25:37 +0500

    Set up of Server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import express from "express"
+const app = express();
+const PORT = process.env.PORT || 5000; 
+
+app.get('/', (req, res) => {
+  res.send('Hello World! This is an Express server on port ' + PORT); 
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`); 
+});
